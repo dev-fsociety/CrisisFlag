fe7f00e653a6aecff1a533a312dc0c3ecb519410
HorlogeSkynet <sam.forestier@gmail.com>
2015-12-03 20:09:22 +0100

    Add AuthComponent + Hash Password + Truncate Password

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -2,6 +2,7 @@
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\Auth\DefaultPasswordHasher;
 
 /**
  * User Entity.
@@ -33,4 +34,12 @@ class User extends Entity
         '*' => true,
         'id' => false,
     ];
+
+    protected function _setPassword($password)
+    {
+        if(strlen($password) > 0)
+        {
+            return (new DefaultPasswordHasher)->hash($password);
+        }
+    }
 }
